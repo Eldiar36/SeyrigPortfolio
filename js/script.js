0109f73fee7d815a139ba2072d558feccb391235
Eldiyar Erkinbekov <eldiarrd@gmail.com>
2020-01-06 05:32:39 +0600

    responsive-design

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
         nav:true,
         navText:["" , ""],
         dots:false,
+        margin:10,
         responsive: {
             0: {
                 items:1,
@@ -27,3 +28,12 @@ $(document).ready(function(){
         }
     });
 });
+$('.mobile-btn').on(  'click',function (e) {
+    e.preventDefault();
+    $('.mobile-btn').toggleClass('active')
+    $('.nav-menu').toggleClass('active')
+})
+$('.nav-menu  li').on('click',function () {
+    $('.mobile-btn').removeClass('active')
+    $('.nav-menu').removeClass('active')
+})
